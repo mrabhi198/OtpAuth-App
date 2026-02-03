2dd49458dfaa9b238677d9877930dafed7615299
Abhinav Alok <abhinavalok318@gmail.com>
2026-02-03 21:30:41 +0530

    OTP manager added with expiry and attemps logic

---
+++ app/src/main/java/com/example/otpauthapp/data/OtpManager.kt
@@ -0,0 +1,34 @@
+package com.example.otpauthapp.data
+
+import kotlin.random.Random
+
+data class OtpData(
+    val otp: String,
+    val timeStamp: Long,
+    var attempts: Int
+)
+
+class OtpManager{
+    private val otpStore= mutableMapOf<String, OtpData>()
+    private val EXPIRY = 60_000L
+    private val MAX_ATTEMPTS = 3
+
+    fun generateOtp(email: String): String{
+        val otp = Random.nextInt(100000, 999999).toString()
+        otpStore[email] = OtpData(otp, System.currentTimeMillis(), 0)
+
+        return otp
+    }
+    fun validateOtp(email : String, input: String): Boolean{
+        val data = otpStore[email]?: return false
+        if(System.currentTimeMillis() -data.timeStamp > EXPIRY){
+            return false
+        }
+
+        if (data.attempts>=MAX_ATTEMPTS){
+            return false
+        }
+        data.attempts++
+        return data.otp == input
+    }
+}
