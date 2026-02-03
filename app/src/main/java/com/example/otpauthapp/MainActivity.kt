522d300a9029aa4777be02b544ee5963c3feda86
Abhinav Alok <abhinavalok318@gmail.com>
2026-02-04 01:05:10 +0530

    completed UI and screen navigation

--- app/src/main/java/com/example/otpauthapp/MainActivity.kt
+++ app/src/main/java/com/example/otpauthapp/MainActivity.kt
@@ -9,16 +9,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.otpauthapp.ui.screen.LoginScreen
+import com.example.otpauthapp.ui.screen.OtpScreen
+import com.example.otpauthapp.ui.screen.SessionScreen
 import com.example.otpauthapp.ui.theme.OtpAuthAppTheme
+import com.example.otpauthapp.viewmodel.AuthViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel = remember { AuthViewModel() }
+            val state = viewModel.state
 
+            when {
+                state.isLoggedIn ->
+                    SessionScreen(state.loginTime, viewModel::logout)
+
+                state.isOtpSent->
+                    OtpScreen(viewModel::verifyOtp,state.error)
+
+                else ->
+                    LoginScreen(viewModel::sendOTP)
+            }
         }
     }
 }
